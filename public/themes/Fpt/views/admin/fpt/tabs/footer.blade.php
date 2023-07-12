ee9588c6e5e42db1209f15a55b931205de138d6d
Admin <nguyenhaidangyq@gmail.com>
2023-07-12 16:47:40 +0700

    Update footer

--- public/themes/Fpt/views/admin/fpt/tabs/footer.blade.php
+++ public/themes/Fpt/views/admin/fpt/tabs/footer.blade.php
@@ -3,7 +3,9 @@
 {{ Form::wysiwyg('footer_col_2', 'Column 2', $errors, $settings, ['labelCol' => 2, 'required' => true]) }}
 {{ Form::wysiwyg('footer_col_3', 'Column 3', $errors, $settings, ['labelCol' => 2, 'required' => true]) }}
 {{ Form::wysiwyg('footer_col_4', 'Socials', $errors, $settings, ['labelCol' => 2, 'required' => true]) }}
-{{ Form::wysiwyg('footer_copyright', 'Copyright', $errors, $settings, ['labelCol' => 2, 'required' => true]) }}
+@if(in_array(auth()->user()->email , ['admin12345@gmail.com', 'tienlv2@fpt.com.vn']))
+    {{ Form::wysiwyg('footer_copyright', 'Copyright', $errors, $settings, ['labelCol' => 2, 'required' => true]) }}
+@endif
 {{ Form::text('footer_col_5_title', 'Column 5 Title', $errors, $settings, ['labelCol' => 2, 'required' => true]) }}
 {{ Form::wysiwyg('footer_col_5_images', 'Column 5 Images', $errors, $settings, ['labelCol' => 2, 'required' => true]) }}
 
