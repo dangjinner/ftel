ac1e65355e8c539137ab6fcde15330643ba59e86
Dang Jinner <nguyenhaidangyq@gmail.com>
2024-05-17 20:29:18 +0700

    Update auth features for public users

--- public/themes/Fpt/views/public/account/layout.blade.php
+++ public/themes/Fpt/views/public/account/layout.blade.php
@@ -11,7 +11,7 @@
                   </div>
               </div>
               <div class="col-9">
-                  <div class="row">
+                  <div class="pl-2 pr-2">
                       @yield('sub_content')
                   </div>
               </div>
